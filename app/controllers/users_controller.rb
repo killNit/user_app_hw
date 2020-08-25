87a4654dd12f9c3c3cf39d401f2e7c2d269fb098
killNit <rileysanti@gmail.com>
2020-08-25 15:20:31 -0600

    user_app1.2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,21 @@
 class UsersController < ApplicationController
   def index
+    @users = Users.all
   end
 
   def show
+    @user = User.find(1)
   end
 
   def new
+    @user = User.new
   end
 
   def edit
+    @user = User.find(1)
   end
 
   def delete
+    @user = User.destroy 
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/show'
-  get 'users/new'
-  get 'users/edit'
-  get 'users/delete'
+ resources :users
+ 
+  # get 'users/index'
+  # get 'users/show'
+  # get 'users/new'
+  # get 'users/edit'
+  # get 'users/delete'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
